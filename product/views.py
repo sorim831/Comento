a7f57e31063e717f7deda2865b1901055b60724f
SeungGyu <sorim831@naver.com>
2025-07-16 19:40:37 +0900

    ✨ product 미리보기 기능 적용

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 #from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import MainContent
 
 #from django.shortcuts import render
@@ -10,3 +10,10 @@ def index(request):
     context = {'content_list':content_list}
     return render(request, 'product/content_list.html',context)
     #return HttpResponse("Hello World")
+
+
+def detail(request, content_id):
+    #content_list = MainContent.objects.get(id=content_id)
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list':content_list}
+    return render(request, 'product/content_detail.html',context)
